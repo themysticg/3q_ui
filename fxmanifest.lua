76aaa2b3a1644afa47e14cb6cd406eab2487e100
The Mystic G <35405638+themysticg@users.noreply.github.com>
2024-09-05 22:09:44 +0100

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+-- https://Discord.me/3qshop
+fx_version 'cerulean'
+game 'gta5'
+
+ui_page 'index.html'
+
+files {
+    'index.html',
+    'styles.css',
+    'script.js',
+}
+
+client_script 'client.lua'
